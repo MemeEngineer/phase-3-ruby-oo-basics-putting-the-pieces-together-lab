2d94c87fca42f8947cd186ab48d10cefa4854b07
Johnny Wu <johnnyw131@gmail.com>
2022-01-11 23:41:54 -0500

    commit tree 555428b40b6002a0b8e6fcabf4292487098765c2

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,21 @@
+class Book
+    attr_accessor :author, :page_count, :genre
+    attr_reader :title
+    
+    def initialize (title)
+        @title = title
+      
+    end
+
+    def turn_page 
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
 
+
+# author, page_count, genre
+# 
+# @author = author
+# @page_count = page_count
+# @genre = genre
